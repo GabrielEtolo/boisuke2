b38388f1bab6d8cac7b98fe0bdd431df712a6375
GabrielEtolo <145966825+GabrielEtolo@users.noreply.github.com>
2024-01-06 03:14:39 +0100

    Update product.vue

--- pages/product.vue
+++ pages/product.vue
@@ -1,32 +1,29 @@
 <template>
   <div class="px-5">
+    <h1 class="text-5xl flex justify-center font-bold mb-10">GALERIE.</h1>
     <ul
       v-if="boisukes"
-      class="
-        grid grid-cols-1
-        sm:grid-cols-2
-        lg:grid-cols-3
-        2xl:grid-cols-5
-        gap-0
-      "
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-0"
     >
-      <!-- Première image avec largeur 1/4 et centrée -->
-
-      <!-- Images suivantes sans espacement -->
       <li
         v-for="(boisuke, index) in boisukes.slice(1)"
         :key="boisuke.id"
-        class="w-full overflow-hidden"
+        class="w-full h-full overflow-hidden fade-in"
+        :style="{ 'animation-delay': index * 0.5 + 's' }"
       >
-        <NuxtImg
-          :src="boisuke.photo.url"
-          :alt="boisuke.title"
-          class="object-cover w-full h-full"
-        />
+        <div class="image-container h-full">
+          <NuxtImg
+            :src="boisuke.photo.url"
+            :alt="boisuke.title"
+            class="object-cover w-full h-full"
+          />
+          <div class="description">{{ boisuke.description }}</div>
+        </div>
       </li>
     </ul>
   </div>
 </template>
+
 <script setup>
 const query = gql`
 query boisukes {
@@ -47,3 +44,40 @@ console.log(data.value);
 boisukes.value = data.value.boisukes;
 </script>
 
+<style>
+.fade-in {
+  animation: fadeIn ease-in 1s;
+  animation-fill-mode: forwards;
+  opacity: 0;
+}
+
+@keyframes fadeIn {
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+.image-container {
+  position: relative;
+}
+
+.image-container:hover .NuxtImg {
+  opacity: 0.3;
+}
+
+.description {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s, opacity 0.5s linear;
+}
+
+.image-container:hover .description {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
